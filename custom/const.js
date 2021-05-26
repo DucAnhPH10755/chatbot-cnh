110d30511c14f4fd08ee7a558f8c861fa9751e33
DucAnhPH10755 <68089802+DucAnhPH10755@users.noreply.github.com>
2021-05-26 16:06:13 +0700

    Update const.js

--- custom/const.js
+++ custom/const.js
@@ -30,5 +30,5 @@ module.exports = {
 	                            //Đặt 0 để cho phép đợi bao lâu cũng đc
 
 	//ADMIN UI
-	ADMIN_PASSWORD : "" //password để login vào trang admin
+	ADMIN_PASSWORD : "anhddph10755" //password để login vào trang admin
 };
